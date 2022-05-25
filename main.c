9a5a7f909e12afeea85adb93eb44c45035b668ec
A Sanath Hoysala <95922278+sanath-hoysala@users.noreply.github.com>
2022-05-25 06:32:01 +0530

    Update main.c

--- main.c
+++ main.c
@@ -1,17 +1,17 @@
 #include<stdio.h> 
-void add_matrix()           // To add the matrix
+void add_matrix(a)           // To add the matrix
 {
 
 }
-void sub_matrix()
+void sub_matrix(a)
 {
 
 }
-void mul_matrix()
+void mul_matrix(a)
 {
 
 }
-void div()
+void div_matrix(a)
 {
 
 }
@@ -20,7 +20,8 @@ struct array                // Data related to an array
 	int r;
 	int c;
 	int ar[100][100];
-}
+}a[100];
+
 
 void get_array()
 {
@@ -28,4 +29,41 @@ void get_array()
 
 int main()
 {
-	struct array; 
+	struct array a[100]; 
+	int n,m,k;
+	printf("To multiply enter : 1 \n To divide enter : 2 \n");
+	scanf("%d",&n);
+	while(k!=0)
+	{
+		printf("Enter the array :\n");
+		while(m!=0)
+		{
+			get_array(a[i]);
+			printf("Do you want to enter another array to operate on if YES enter '1' and if NO enter '0':\n");
+			scanf("%d\n",&m);
+			i++;
+		}
+		if(n==1)
+			mul_martix(a);
+		else if(n==2)
+			div_matrix(a);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
