0c1e539c33bcd7fa07475b1ddc2e03fb91e6e1ac
A Sanath Hoysala <sanath.hoysala@gmail.com>
2022-06-07 21:07:28 +0530

    Updated add,sub,init,get_array functions -7/6/2022

--- main.c
+++ main.c
@@ -1,18 +1,147 @@
 #include<stdio.h> 
-int n,i;                                      // Global variable
+int n,i=0;                                    // Global variable
 struct array                                  // Data related to an array 
 {
 	int r;
 	int c;
 	int ar[100][100];
-}a[100],s[1];                            
+}a[100],s[1];  
+// Declaring all the functions 
+void add_matrix(struct array a[i]);  
+void sub_matrix(struct array a[i]);
+void mul_matrix(struct array a[i]);
+void div_matrix(struct array a[i]);
+void invert(struct array a[i]);
+void get_array(int n);
+void initial(int n); 
+// -----------------------------------------------------------------------------------------------------------------------               
+int main()
+{
+	struct array a[100]; 
+	int o=0;
+	initial(1);
+	while(True):
+	{
+		if(o=0)
+			get_array(1);
+			o++;
+		else
+		{
+			printf("You can do the following operations on the matrices --\n
+			1. Addition \n
+			2. Subtraction \n
+			3. Multiplication \n
+			4. Division \n
+			5. Transpose \n
+			6. Scalar multiplication\n");
+			scanf(&n);
+			switch(n)
+			{
+				case 1 : add_matrix();
+						break;
+				case 2 : sub_matrix();
+						break;
+				case 3 : mul_matrix();
+						break;
+				case 4 : div_matrix();
+						break;
+				case 5 : invert();
+						break;
+				default : exit(0);
+			}
+		}
+	}
+}
+
+
+
+// ---------------------------------------------------------------------------------------------------------------------
+void initial(int n)                                     // To initialize 
+{
+	printf("You can do the following operations on the matrices\n
+	1. Addition \n
+	2. Subtraction \n
+	3. Multiplication \n
+	4. Division \n
+	5. Transpose \n
+	6. Scalar multiplication\n");
+	
+}
+void get_array(int n)
+{
+	if(n==1)
+	{
+		scanf("Enter the size of row and column of first matrix: %d %d", &a[i].r, &a[i].c);   // Getting first matrix
+		for(int c=0; c<a[i].c ; c++)
+		{
+			for(int r=0 ; r<a[i].r ; r++)
+			{
+				printf("Enter the value for a[%d][%d] : ",r,c);
+				scanf(&a[i].ar[r][c]);
+			}
+		}
+		scanf("Enter the size of row and column of second matrix: %d %d", &a[i].r, &a[i].c);  // Getting second matrix
+		for(int c=0; c<a[i+1].c ; c++)
+		{
+			for(int r=0 ; r<a[i+1].r ; r++)
+			{
+				printf("Enter the value for a[%d][%d] : ",r,c);
+				scanf(&a[i+1].ar[r][c]);
+			}
+		}
+	}
+	else if(n==2)
+		{
+			for(int c=0; c<a[i].c ; c++)
+			{
+				for(int r=0 ; r<a[i].r ; r++)
+				{
+					a[i+1].ar[r][c]=s[i].ar[r][c];
+				}
+			}
+			
+		}
+	else
+	{
+		printf("Invalid input");
+		initial(1);
+	}
+
+}
 void add_matrix(struct array a[i] )           // To add the matrices
 {
+	if(a[i].r==a[i+1].r && a[i].c==a[i+1].c)
+	{
+		for(int c=0; c<a[i].c ; c++)
+		{
+			for(int r=0 ; r<a[i].r ; r++)
+			{
+				s[0].ar[r][c]=a[i].ar[r][c]+a[i+1].ar[r][c];
+			}
+		}
+	}
+	else
+	{
+		printf("Both matrix should be of same order\n");
+	}
 
 }
 void sub_matrix(struct array a[i])            // To subtract matrices
 {
-
+	if(a[i].r==a[i+1].r && a[i].c==a[i+1].c)
+	{
+		for(int c=0; c<a[i].c ; c++)
+		{
+			for(int r=0 ; r<a[i].r ; r++)
+			{
+				s[0].ar[r][c]=a[i].ar[r][c]-a[i+1].ar[r][c];
+			}
+		}
+	}
+	else
+	{
+		printf("Both matrix should be of same order\n");
+	}
 }
 void mul_matrix(struct array a[i])            // To multiply matrices
 {
@@ -39,47 +168,9 @@ void invert(struct array a[i])
 {
 
 }
-void initial(n)                                // To initialize 
-{
-	printf("Select one of the following options :- \n
-	1) Input a matrix \n
-	2) Operate on previous answer \n");
-	scanf("%d",&n);
-	if(n==1)
-		get_array();
-	else if(n==2)
-	{
-
-	}
-	else	
-		{
-			printf("Invalit input");
-			initial(n);
-		}
-		
-}
-void get_array(struct array a[i])
-{
-	scanf("Enter the size of row and column : %d %d", &a[i].r, &a[i].c);
-	for(int c=0; c<a[i].c ; c++)
-	{
-		for(int r=0 ; r<a[i].r ; r++)
-		{
-			printf("Enter the value for a[%d][%d] : ",r,c);
-			scanf(&a[i].ar[r][c]);
-		}
-	}
-
-}
-int main()
-{
-	struct array a[100]; 
-	initial(n);
-
-		
 
 
-}
+
 
 
 
